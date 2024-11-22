bdf346168bf172be3fa3f6402aaad83ee28da6be
이영빈 <tina4686@gmail.com>
2024-11-22 13:50:11 +0900

    start spring project

---
+++ src/main/java/haedal/j4/knuseminar/KnuseminarApplication.java
@@ -0,0 +1,13 @@
+package haedal.j4.knuseminar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnuseminarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnuseminarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/haedal/j4/knuseminar/KnuseminarApplicationTests.java
@@ -0,0 +1,13 @@
+package haedal.j4.knuseminar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnuseminarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
